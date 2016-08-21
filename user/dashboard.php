e2394efe05387e5abcb40ea74f41fe029b27faf1
guiduan <254732500@qq.com>
2016-08-21 18:52:46 +0800

    Create dashboard.php

---
+++ user/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'includes/chkLogin.php';
+if ( ! isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ) {
+	exit;
+}
+if ( $_SERVER['REQUEST_METHOD'] != 'POST' ) {
+	exit;
+}
+if ( $_POST['action'] != 'true' ) {
+	exit;
+}
+require_once '../includes/config.php';
+try {
+	$link = new PDO( RAD_DB_DRIVER . ':host=' . RAD_DB_HOST . ';dbname=' . RAD_DB_NAME, RAD_DB_USER, RAD_DB_PASS );
+} catch ( PDOException $Exception ) {
+	die( $Exception->getMessage() );
+}
+require_once 'includes/functions.php';
+$userTotalUploadTraffic = userTotalUploadTraffic($_SESSION['USERNAME']);
+$userTotalDownloadTraffic = userTotalDownloadTraffic($_SESSION['USERNAME']);
+$userTotalOnlineTime = userTotalOnlineTime($_SESSION['USERNAME']);
