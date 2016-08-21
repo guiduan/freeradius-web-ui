861798af50fcb3a99f8588457c8557cdd8c17d2a
guiduan <254732500@qq.com>
2016-08-21 14:11:12 +0800

    Update menu.php

--- core/includes/menu.php
+++ core/includes/menu.php
@@ -5,8 +5,8 @@ if ( preg_match( '/menu.php/', $_SERVER['SCRIPT_NAME'] ) ) {
 }
 ?>
 
-<header class="bg-dark">
-	<div class="navigation-bar dark">
+<header class="bg-blue">
+	<div class="navigation-bar blue">
 		<div class="navigation-bar-content container">
 			<a href="javascript:void(0);" class="element">
 				<span class="icon-grid-view"></span>
@@ -18,25 +18,25 @@ if ( preg_match( '/menu.php/', $_SERVER['SCRIPT_NAME'] ) ) {
 				<li>
 					<a href="javascript:void(0);" onclick="redirect('./')">
 						<span class="icon-dashboard"></span>
-						<span>Dashboard</span>
+						<span>控制面板</span>
 					</a>
 				</li>
 				<li class="element-divider"></li>
 				<li>
 					<a class="dropdown-toggle" href="javascript:void(0);">
 						<span class="icon-user-2"></span>
-						<span>Manage Users</span>
+						<span>用户管理</span>
 					</a>
 					<ul class="dropdown-menu" data-role="dropdown">
 						<li>
 							<a href="javascript:addNewUserDialog();">
-								<span>New User</span>
+								<span>新增用户</span>
 							</a>
 						</li>
 						<li class="divider"></li>
 						<li>
 							<a href="javascript:void(0);" onclick="javascript:redirect('./users.php')">
-								<span>List Users</span>
+								<span>用户列表</span>
 							</a>
 						</li>
 					</ul>
@@ -45,18 +45,18 @@ if ( preg_match( '/menu.php/', $_SERVER['SCRIPT_NAME'] ) ) {
 				<li>
 					<a class="dropdown-toggle" href="javascript:void(0);">
 						<span class="icon-monitor-2"></span>
-						<span>Manage NAS</span>
+						<span>NAS管理</span>
 					</a>
 					<ul class="dropdown-menu" data-role="dropdown">
 						<li>
 							<a href="javascript:void(0);" onclick="javascript:addNewNasDialog();">
-								<span>New NAS</span>
+								<span>新增NAS</span>
 							</a>
 						</li>
 						<li class="divider"></li>
 						<li>
 							<a href="javascript:void(0);" onclick="javascript:redirect('./nas.php')">
-								<span>List NAS</span>
+								<span>NAS列表</span>
 							</a>
 						</li>
 					</ul>
@@ -69,9 +69,9 @@ if ( preg_match( '/menu.php/', $_SERVER['SCRIPT_NAME'] ) ) {
 				<div class="element place-right">
 					<a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-cog"></span></a>
 					<ul class="dropdown-menu place-right" data-role="dropdown">
-						<li><a href="javascript:void(0);" onclick="changePasswordDialog();">Change Password</a></li>
+						<li><a href="javascript:void(0);" onclick="changePasswordDialog();">密码修改</a></li>
 						<li class="divider"></li>
-						<li><a href="javascript:void(0);" onclick="javascript:redirect('./logout.php?action=true');">Sign Out</a></li>
+						<li><a href="javascript:void(0);" onclick="javascript:redirect('./logout.php?action=true');">登出</a></li>
 					</ul>
 				</div>
 				<span class="element-divider place-right"></span>
