1f89e9fe6f4efb00d837570f47709f82451d3435
guiduan <254732500@qq.com>
2016-08-21 14:20:23 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,11 +5,11 @@ if ( preg_match( "/footer.php/", $_SERVER['SCRIPT_NAME'] ) ) {
 }
 ?>
 
-<footer class="bg-dark" style="position: fixed; bottom: 0; width: 100%; height: 40px;">
+<footer class="bg-blue" style="position: fixed; bottom: 0; width: 100%; height: 40px;">
 	<div style="padding: 8px;">
 		<p>
 			<span class="place-right" style="padding-right: 50px; color: #ffffff;">
-				Made with <i class="icon-heart"></i> by: <a href="https://gitlab.com/groups/arch-lamp" style="color: #ffffff;">arch-lamp</a>
+				Made with <i class="icon-heart"></i> by: <a href="https://gitlab.com/groups/arch-lamp" style="color: #ffffff;">DoTunnel</a>
 			</span>
 		</p>
 	</div>
