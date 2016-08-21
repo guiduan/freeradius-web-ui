77476398aae306eb48b38bf0b089f4e5e071cf45
guiduan <254732500@qq.com>
2016-08-21 19:07:49 +0800

    Update dashboard.php

--- user/dashboard.php
+++ user/dashboard.php
@@ -18,7 +18,6 @@ try {
 require_once 'includes/functions.php';
 $userTotalUploadTraffic = userTotalUploadTraffic($_SESSION['USERNAME']);
 $userTotalDownloadTraffic = userTotalDownloadTraffic($_SESSION['USERNAME']);
-$userTotalTraffic = $userTotalUploadTraffic + $userTotalDownloadTraffic;
 $userTotalOnlineTime = userTotalOnlineTime($_SESSION['USERNAME']);
 
 
@@ -26,7 +25,6 @@ $userTotalOnlineTime = userTotalOnlineTime($_SESSION['USERNAME']);
 $info = array(
 	'userTotalUploadTraffic'=>$userTotalUploadTraffic,
 	'userTotalDownloadTraffic'=>$userTotalDownloadTraffic,
-	'userTotalTraffic'=>$userTotalTraffic,
 	'userTotalOnlineTime'=>$userTotalOnlineTime,
 );
 print json_encode($info);
