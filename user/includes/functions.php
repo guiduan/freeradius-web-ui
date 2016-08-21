a72ca123b366e31314d42d50cb644080dbde7416
guiduan <254732500@qq.com>
2016-08-21 17:44:11 +0800

    Update functions.php

--- user/includes/functions.php
+++ user/includes/functions.php
@@ -6,6 +6,7 @@ if ( preg_match( "/functions.php/", $_SERVER['SCRIPT_NAME'] ) ) {
 ?>
 
 <?php
+//用户总上传流量
 function userTotalUploadTraffic($username='') {
   global $link;
   $sql = "SELECT SUM(acctoutputoctets) AS user_total_upload_traffic FROM radacct WHERE username="."$username";
@@ -14,7 +15,7 @@ function userTotalUploadTraffic($username='') {
 	$traffic = $userTotalUploadTraffic->fetch( PDO::FETCH_OBJ );
 	return $traffic->user_total_upload_traffic;
 }
-
+//用户总下载流量
 function userTotalDownloadTraffic($username='') {
   global $link;
   $sql = "SELECT SUM(acctinputoctets) AS user_total_download_traffic FROM radacct WHERE username="."$username";
@@ -23,3 +24,13 @@ function userTotalDownloadTraffic($username='') {
 	$traffic = $userTotalDownloadTraffic->fetch( PDO::FETCH_OBJ );
 	return $traffic->user_total_download_traffic;
 }
+//用户总在线时间:单位：s
+function userTotalOnlineTime($username='') {
+  global $link;
+  $sql = "SELECT SUM(acctsessiontime) AS user_total_online_time FROM radacct WHERE username="."$username";
+	$userTotalOnlineTime = $link->prepare($sql);
+	$userTotalOnlineTime->execute();
+	$time = $userTotalOnlineTime->fetch( PDO::FETCH_OBJ );
+	return $time->user_total_online_time;
+}
+
